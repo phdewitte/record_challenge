7b32e0ef6756f9e8bb485eb20713c5050f5630bf
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-08-11 16:41:15 -0500

    Artist object display formatting

--- artist.rb
+++ artist.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Artist
 
   attr_reader :first_name, :last_name, :gender, :fav_color, :birthdate
@@ -8,6 +10,14 @@ class Artist
     @gender = artist_attributes[:gender]
     @fav_color = artist_attributes[:fav_color]
     @birthdate = artist_attributes[:birthdate]
+  end
+
+  def formatted_bday
+    Date.parse(@birthdate).strftime('%e %B, %Y')
+  end
+
+  def display_info
+    "Name: #{first_name} #{last_name} Gender: #{gender} Favorite Color: #{fav_color} Birthdate: #{formatted_bday}"
   end
 
 end
--- artist_spec.rb
+++ artist_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'artist'
 
 describe Artist do
-  let(:artist) { Artist.new(first_name: "Hank", last_name: "Williams", gender: "male", fav_color: "brown", birthdate: "1923-17-09") }
+  let(:artist) { Artist.new(first_name: "Hank", last_name: "Williams", gender: "Male", fav_color: "Brown", birthdate: "1923-09-17") }
 
   context "#initialize" do
     it "assigns the artist a first name" do
@@ -13,15 +13,27 @@ describe Artist do
     end
 
     it "assigns the artist a gender" do
-      expect(artist.gender).to eq "male"
+      expect(artist.gender).to eq "Male"
     end
 
     it "assigns the artist a favorite color" do
-      expect(artist.fav_color).to eq "brown"
+      expect(artist.fav_color).to eq "Brown"
     end
 
     it "assigns the artist a birthdate" do
-      expect(artist.birthdate).to eq "1923-17-09"
+      expect(artist.birthdate).to eq "1923-09-17"
+    end
+  end
+
+  context "#formatted_bday" do
+    it "displays the artist's birthdate in a proper format" do
+      expect(artist.formatted_bday).to eq "17 September, 1923"
+    end
+  end
+
+  context "#display_info" do
+    it "displays all the artist's information in a readable format" do
+      expect(artist.display_info).to eq "Name: Hank Williams Gender: Male Favorite Color: Brown Birthdate: 17 September, 1923"
     end
   end
 
